9ca4b4162192650bbbeca65814c6f75ce8cc0c4e
Ryry012 <sadartifact25@gmail.com>
2025-02-03 15:10:47 -0800

    done

--- class/battle_dice/charactertype.py
+++ class/battle_dice/charactertype.py
@@ -1,6 +1,15 @@
-from enum import enum 
+from enum import Enum
 
-class characterType(Enum):
-    WARRIOR = " warrior"
+class CharacterType(Enum):
+    WARRIOR = "warrior"
     MAGE = "Mage"
-    ROGUE = " Rogue"
+    ROGUE = "Rogue"
+
+from charactertype import CharacterType
+
+# Accessing the CharacterType Enum
+my_character_type = CharacterType.WARRIOR
+
+print(my_character_type)        # Output: CharacterType.WARRIOR
+print(my_character_type.name)   # Output: "WARRIOR"
+print(my_character_type.value)  # Output: "warrior"
--- class/battle_dice/game.py
+++ class/battle_dice/game.py
@@ -1,12 +1,14 @@
-import random 
-from character import character
+import random
+from character import Character
+from charactertype import CharacterType
 
-class Game: 
-    def__init__(self, player1: CHaracter. player2:character):
-    self.__player1 = player1 
-    self.__player2 = player2
+class Game:
+    def __init__(self, player1: Character, player2: Character):
+        self.__player1 = player1
+        self.__player2 = player2
 
-def attack(self, attacker: character, defender: character):
-    pass # TODO implement the battle loop where players tale turns attacking
+    def attack(self, attacker: Character, defender: Character):
+        pass  # TODO: Implement the battle loop where players take turns attacking
 
- 
+    def start_battle(self):
+        pass  # TODO: Implement the battle logic
