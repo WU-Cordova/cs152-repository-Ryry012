076bda208ca5fe22dfeab28237ce2eb4c5feed8a
Ryry012 <sadartifact25@gmail.com>
2025-04-07 22:36:44 -0700

    done

--- datastructures/arraystack.py
+++ datastructures/arraystack.py
@@ -1,37 +1,59 @@
 import os
-from datastructures.array import Array, T
-from datastructures.istack import IStack
+from typing import Iterator, TypeVar, Generic, List
+
+T = TypeVar('T')
+
+class Array(Generic[T]):
+    def __init__(self, size: int, data_type: type = object) -> None:
+        self._data = [None] * size
+        self._data_type = data_type
+
+    def __getitem__(self, index: int) -> T:
+        return self._data[index]
+
+    def __setitem__(self, index: int, value: T) -> None:
+        self._data[index] = value
+
+class IStack(Generic[T]):
+    def push(self, item: T) -> None:
+        raise NotImplementedError
+
+    def pop(self) -> T:
+        raise NotImplementedError
 
 class ArrayStack(IStack[T]):
-    def __init__(self, max_size: int = 0, data_type=object) -> None:
+    def __init__(self, max_size: int = 0, data_type: type = object) -> None:
         self._maxsize = max_size
-        self.stack = Array(max_size, data_type)
-        self._top = 0  # Points to the next empty index
+        self._stack = Array(max_size, data_type)
+        self._top = 0
+        self._data_type = data_type
 
     def push(self, item: T) -> None:
+        if not isinstance(item, self._data_type):
+            raise TypeError(f"Expected {self._data_type}, got {type(item)}")
         if self.full:
             raise IndexError('Stack is full')
-        self.stack[self._top] = item
+        self._stack[self._top] = item
         self._top += 1
 
     def pop(self) -> T:
         if self.empty:
             raise IndexError('Stack is empty')
         self._top -= 1
-        item = self.stack[self._top]
-        self.stack[self._top] = None  # Optional: Clear reference
+        item = self._stack[self._top]
+        self._stack[self._top] = None
         return item
 
     def clear(self) -> None:
         for i in range(self._top):
-            self.stack[i] = None
+            self._stack[i] = None
         self._top = 0
 
     @property
     def peek(self) -> T:
         if self.empty:
             raise IndexError('Stack is empty')
-        return self.stack[self._top - 1]
+        return self._stack[self._top - 1]
 
     @property
     def maxsize(self) -> int:
@@ -39,37 +61,33 @@ class ArrayStack(IStack[T]):
 
     @property
     def full(self) -> bool:
-        return self._top == self._maxsize
+        return self._maxsize > 0 and self._top == self._maxsize
 
     @property
     def empty(self) -> bool:
         return self._top == 0
 
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, ArrayStack):
-            return False
-        if self._top != other._top:
-            return False
-        for i in range(self._top):
-            if self.stack[i] != other.stack[i]:
-                return False
-        return True
+    def __iter__(self) -> Iterator[T]:
+        for i in reversed(range(self._top)):
+            yield self._stack[i]
 
     def __len__(self) -> int:
         return self._top
 
     def __contains__(self, item: T) -> bool:
-        for i in range(self._top):
-            if self.stack[i] == item:
-                return True
-        return False
+        return any(x == item for x in self)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ArrayStack):
+            return False
+        return list(self) == list(other)
 
     def __str__(self) -> str:
-        return str([self.stack[i] for i in range(self._top)])
+        return str(list(self))
 
     def __repr__(self) -> str:
-        return f"ArrayStack({self.maxsize}): items: {str(self)}"
+        return f"ArrayStack(maxsize={self.maxsize}, items={list(self)})"
 
 if __name__ == '__main__':
     filename = os.path.basename(__file__)
-    print(f'OOPS!\nThis is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
+    print(f'This is {filename}. Run your tests instead.')
--- datastructures/circularqueue.py
+++ datastructures/circularqueue.py
@@ -4,46 +4,49 @@ from datastructures.iqueue import IQueue
 
 T = TypeVar('T')
 
-class CircularQueue:
-    def __init__(self, maxsize):
+class CircularQueue(IQueue[T]):
+    def __init__(self, maxsize: int) -> None:
         self.maxsize = maxsize
-        self.queue = [None] * maxsize
+        self.queue: Array[T] = Array(maxsize, object)
         self.front = 0
         self.rear = 0
         self.size = 0
 
-    def enqueue(self, item):
-        if self.size == self.maxsize:
+    def enqueue(self, item: T) -> None:
+        if self.is_full():
             raise IndexError("Queue is full")
         self.queue[self.rear] = item
         self.rear = (self.rear + 1) % self.maxsize
         self.size += 1
 
-    def dequeue(self):
-        if self.size == 0:
+    def dequeue(self) -> T:
+        if self.is_empty():
             raise IndexError("Queue is empty")
         item = self.queue[self.front]
         self.front = (self.front + 1) % self.maxsize
         self.size -= 1
         return item
 
-    def peek(self):
-        if self.size == 0:
+    def peek(self) -> T:
+        if self.is_empty():
             raise IndexError("Queue is empty")
         return self.queue[self.front]
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.size == 0
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return self.size == self.maxsize
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size
 
-    def __str__(self):
+    def __str__(self) -> str:
         items = []
         for i in range(self.size):
             index = (self.front + i) % self.maxsize
-            items.append(self.queue[index])
-        return str(items)
+            items.append(str(self.queue[index]))
+        return f"[{', '.join(items)}]"
+
+    def __repr__(self) -> str:
+        return f"CircularQueue(maxsize={self.maxsize}, size={self.size})"
